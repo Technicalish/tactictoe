52f7e98eba9aaf54a89fdcec8c8b6ece921c5642
Technicalish <raisashahg@gmail.com>
2022-10-26 08:46:55 +0530

    sks

--- server.js
+++ server.js
@@ -45,63 +45,6 @@ var matchFound;
   mySocket.join(roomId);
   console.log(chalk.green(`New room created: ${roomId}`));
   }
-  
 })
-/*io.on("connection", (socket) => {
-  socket.on("iwannaplay", async () => {
-  await jumbleDb.open();
-  await jumbleDb.put(socket.id, "");
-  await jumbleDb.close();
-  var chunk = (arr, size) => {
-  var chunkedArr = [];
-    for (let i = 0; i < arr.length; i += size) {
-    chunkedArr.push(arr.slice(i, i + size));
-    }
-  return chunkedArr;
-  }
-  await jumbleDb.open();
-  db = await jumbleDb.keys().all();
-  await jumbleDb.close();
-  db = chunk(db, 2);
-  console.log(db)
-    db.forEach(pair => {
-      if (pair.length === 2) {
-      var roomId = require("uuid").v4();
-      var xOo = ["x", "o"];
-      var bob = (arr) => {
-      return arr.splice(Math.floor(Math.random()*arr.length), 1);
-      }
-        pair.forEach(async (id, i) => {
-        var toss = bob(xOo)[0];
-        io.to(id).emit("wishgranted", roomId, pair[pair.length-1-i], toss);
-        await jumbleDb.open();
-        await jumbleDb.del(id);
-        await jumbleDb.close();
-        });
-      }
-    });
-  });
-  socket.on("joinroom", (roomId) => {
-  socket.join(roomId);
-  })
-  socket.on("disconnecting", () => {
-    for (let room of socket.rooms) {
-      if (room !== socket.id) {
-      socket.to(room).emit("userleft", socket.id);
-        io.in(room).fetchSockets()
-        .then(roomUsers => {
-        console.log("map", roomUsers.map(u => u.id))
-          roomUsers.forEach(async user => {
-            if (user.id !== socket.id) {
-            await jumbleDb.open();
-            await jumbleDb.put(user.id, "");
-            await jumbleDb.close();
-            }
-          });
-        });
-      }
-    }
-  });
-});*/
 app.use(express.static("./public"));
 server.listen(process.env.PORT || 3000);
